681b0c9d6592cbf2d3fdc30a4ab7774bab0e93dc
Breno Costa <brenocostanascimento031@gmail.com>
2026-01-23 18:58:20 -0300

    Bug na Build solucionado

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useFonts } from 'expo-font';
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+
+// Impede a splash de sumir automaticamente
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -8,24 +13,30 @@ export default function RootLayout() {
     'JetBrainsMono-Medium': require('../assets/fonts/JetBrainsMono-Medium.ttf'),
     'JetBrainsMono-Italic': require('../assets/fonts/JetBrainsMono-Italic.ttf'),
     'JetBrainsMono-Bold': require('../assets/fonts/JetBrainsMono-Bold.ttf'),
-  });
-  
-  if (!fontsLoaded) {
-      return null; // ou SplashScreen
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
     }
-  
+  }, [fontsLoaded])
+
+  // Enquanto carrega fontes, mantém a splash
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#E6E6E6" }} edges={['top']}>
-      <Stack >
-        <Stack.Screen 
-          name="(tabs)" 
-          options={{ 
-            headerShown: false, 
-            statusBarStyle: 'dark'
-          }} 
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+            statusBarStyle: 'dark',
+          }}
         />
       </Stack>
     </SafeAreaView>
-    
   )
 }
