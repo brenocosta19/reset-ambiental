c8d3c1c3ed1a892fee8a2d6247cc6981937875f8
Breno Costa <brenocostanascimento031@gmail.com>
2026-01-23 17:37:09 -0300

    Updt index file

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         },
       }}>
         <Tabs.Screen 
-          name="home"
+          name="index"
           
           options={{
             tabBarIcon: ({ color, focused }) => (
